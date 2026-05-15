ac2eb611384d08d15d08bb8b15f6d3b65ff56ace
rehnahn-max <rehnahn@gmail.com>
2026-05-15 18:31:13 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,3 @@
-const CACHE_NAME = 'renan-tech-v1';
-
-const urlsToCache = [
-'./',
-'./index.html',
-'./Fundo.jpg'
-];
-
-self.addEventListener('install', event => {
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache => cache.addAll(urlsToCache))
-);
-});
-
-self.addEventListener('fetch', event => {
-event.respondWith(
-caches.match(event.request)
-.then(response => response || fetch(event.request))
-);
+self.addEventListener('install', e => {
+  console.log('App instalado');
 });
