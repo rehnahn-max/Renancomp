59cc849dcfb9aeaada466dcac7bd271f5bf51ed6
rehnahn-max <rehnahn@gmail.com>
2026-05-15 23:57:14 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,108 @@
-const CACHE_NAME = 'renan-tech-v1';
+const CACHE_NAME = 'renan-tech-v10';
 
 const urlsToCache = [
-'./',
-'./index.html',
-'./Fundo.jpg'
+
+'/',
+'/index.html',
+'/manifest.json',
+'/Fundo.jpg',
+'/icon-192.png',
+'/icon-512.png'
+
 ];
 
+/* INSTALL */
+
 self.addEventListener('install', event => {
+
+self.skipWaiting();
+
 event.waitUntil(
+
 caches.open(CACHE_NAME)
-.then(cache => cache.addAll(urlsToCache))
+
+.then(cache => {
+
+return cache.addAll(urlsToCache);
+
+})
+
 );
+
 });
 
-self.addEventListener('fetch', event => {
-event.respondWith(
-caches.match(event.request)
-.then(response => response || fetch(event.request))
+/* ACTIVATE */
+
+self.addEventListener('activate', event => {
+
+event.waitUntil(
+
+caches.keys().then(keys => {
+
+return Promise.all(
+
+keys.map(key => {
+
+if(key !== CACHE_NAME){
+
+return caches.delete(key);
+
+}
+
+})
+
 );
+
+})
+
+);
+
+return self.clients.claim();
+
+});
+
+/* FETCH */
+
+self.addEventListener('fetch', event => {
+
+event.respondWith(
+
+fetch(event.request)
+
+.then(response => {
+
+const responseClone = response.clone();
+
+caches.open(CACHE_NAME)
+
+.then(cache => {
+
+cache.put(event.request, responseClone);
+
+});
+
+return response;
+
+})
+
+.catch(() => {
+
+return caches.match(event.request);
+
+})
+
+);
+
+});
+
+/* FORCE UPDATE */
+
+self.addEventListener('message', event => {
+
+if(event.data === 'skipWaiting'){
+
+self.skipWaiting();
+
+}
+
 });
